5fa6b8544a60819936edcb9fd01e4dbfb75ede8a
aryanagrawal22 <aryanagrawal20023@gmail.com>
2022-03-18 01:47:09 +0530

    TypeGraphQL & Express: Setup completed

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,14 +1,33 @@
 import {MikroORM} from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/post";
 import microConfig from "./mikro-orm.config";
+import express from "express";
+import {ApolloServer} from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async() => {
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up();
 
-    const post = orm.em.fork({}).create(Post, {title: "my second post"});
-    await orm.em.persistAndFlush(post);
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        })
+    });
+
+    await apolloServer.start();
+
+    apolloServer.applyMiddleware({app});
+
+    app.listen (4000, () => {
+        console.log('Server started at port: 4000')
+    })
 }
 
-main();
+main().catch((err) => {
+    console.error(err);
+  });
