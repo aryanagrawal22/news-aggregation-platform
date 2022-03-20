df69d54d281fa66603c236af90e940505678b94e
aryanagrawal22 <aryanagrawal20023@gmail.com>
2022-03-20 13:28:38 +0530

    Register: Completed

--- api/src/mikro-orm.config.ts
+++ api/src/mikro-orm.config.ts
@@ -2,13 +2,14 @@ import { __prod__ } from "./constants";
 import { Post } from "./entities/post";
 import { Options } from '@mikro-orm/core';
 import path from "path";
+import { User } from "./entities/user";
 
 const config: Options = {
     migrations: {
         path: path.join(__dirname, './migrations'), 
         glob: '!(*.d).{js,ts}',
     },
-    entities: [Post],
+    entities: [Post, User],
     dbName: 'news_aggregation',
     type: 'postgresql',
     debug:  !__prod__,
--- api/src/resolvers/post.ts
+++ api/src/resolvers/post.ts
@@ -1,6 +1,6 @@
 import { Post } from "../entities/post";
 import { MyContext } from "src/types";
-import { Arg, Ctx, Int, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
 
 @Resolver()
 export class PostResolver {
@@ -17,5 +17,30 @@ export class PostResolver {
         @Ctx() {em}: MyContext
     ) {
         return em.findOne(Post, {id});
+    }
+
+    @Mutation(()=> Post)
+    async createPost(
+        @Arg('title', ()=> String) title: string,
+        @Ctx() {em}: MyContext
+    ) {
+        const post = em.create(Post, {title})
+        await em.persistAndFlush(post);
+        return post;
+    }
+
+    @Mutation(()=> Post, {nullable: true})
+    async updatePost(
+        @Arg('id', ()=> Int) id: number ,
+        @Arg('title', ()=> String) title: string,
+        @Ctx() {em}: MyContext
+    ): Promise<Post | null> {
+        const post = await em.findOne(Post, {id})
+        if(!post){
+            return null;
+        }
+        post.title = title;
+        await em.persistAndFlush(post);
+        return post;
     }
 }
