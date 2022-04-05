47ba99937c33ace11155874142bdca640d1c95aa
aryanagrawal22 <aryanagrawal20023@gmail.com>
2022-04-05 23:03:41 +0530

    Login added

--- api/src/resolvers/user.ts
+++ api/src/resolvers/user.ts
@@ -1,9 +1,9 @@
 import { User } from "../entities/user";
 import { MyContext } from "src/types";
-import { Arg, Ctx, Field, InputType, Mutation, Resolver } from "type-graphql";
+import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Resolver } from "type-graphql";
 import argon2 from "argon2";
 
-
+// InputType is used in arguments (@args)
 @InputType()
 class UsernamePasswordInput {
   @Field()
@@ -12,6 +12,26 @@ class UsernamePasswordInput {
   password: string;
 }
 
+// ObjectType is used as return type of mutations (@mutations)
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse{
+  @Field(()=> [FieldError], {nullable: true})
+  errors?: FieldError[];
+
+  @Field(()=> User, {nullable: true})
+  user?: User
+}
+
+// REGISTER RESOLVER
 @Resolver()
 export class UserResolver {
   @Mutation(() => User)
@@ -26,5 +46,42 @@ export class UserResolver {
     });
     await em.persistAndFlush(user);
     return user;
+  }
+
+
+  // LOGIN RESOLVER
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    
+    const user = await em.findOne(User, {username: options.username});
+
+    if(!user){
+      return {
+        errors: [{
+          field: "username",
+          message: "that username dosen't exist",
+        },],
+      };
+    }
+
+    const valid = await argon2.verify(user.password, options.password);
+
+    if(!valid){
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "incorrect password",
+          }
+        ]
+      }
+    }
+    
+    return{
+      user,
+    } ;
   }
 }
